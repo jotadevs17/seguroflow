78a1c044101bae52d42cecf34c7ab66f7e43a603
Jota <joao_flap@hotmail.com>
2026-06-04 22:45:40 -0300

    chore: initialize Spring Boot policy api

---
+++ policy-api/src/main/java/com/seguroflow/policyapi/PolicyApiApplication.java
@@ -0,0 +1,13 @@
+package com.seguroflow.policyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyApiApplication.class, args);
+	}
+
+}
